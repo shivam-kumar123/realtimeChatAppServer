2d023e9f6bb1b5191e0144d38f0cf687cc4916d9
shivam-kumar <20051252@kiit.ac.in>
2023-04-23 17:42:20 +0530

    roomCount / appCount feature added to server-side

--- index.js
+++ index.js
@@ -8,21 +8,28 @@ require("dotenv").config()
 
 app.use(cors());
 
+let userCount = 0
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "https://realtimechatappclient.onrender.com",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
+  userCount++
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log(`userCount: ${userCount} User with ID: ${socket.id} joined room: ${data}`);
+    const clientsInRoom = io.sockets.adapter.rooms.get(data)?.size ?? 0;
+    io.to(data).emit("room_count", clientsInRoom);
+    io.emit("user_count", userCount);
   });
 
   socket.on("send_message", (data) => {
@@ -30,7 +37,9 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
+    userCount--
     console.log("User Disconnected", socket.id);
+    io.emit("user_count", userCount);
   });
 });
 
