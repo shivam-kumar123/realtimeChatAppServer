529f674fc9af33c29f43a9c0f5ea68fde7c302c6
shivam-kumar <20051252@kiit.ac.in>
2023-04-26 16:01:53 +0530

    ENTER feature added , Image left

--- index.js
+++ index.js
@@ -8,36 +8,41 @@ require("dotenv").config()
 
 app.use(cors());
 
-let userCount = 0
+// let userCount = 0
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://realtimechatappclient.onrender.com",
-    // origin: "http://localhost:3000",
+    // origin: "https://realtimechatappclient.onrender.com",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  userCount++
-  console.log(`User Connected: ${socket.id}`);
+
+  socket.emit("your_id", socket.id)
+
+  let userCount = io.engine.clientsCount
+  // userCount++
+  console.log(`User Connected: ${socket.id}`)
 
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`userCount: ${userCount} User with ID: ${socket.id} joined room: ${data}`);
     const clientsInRoom = io.sockets.adapter.rooms.get(data)?.size ?? 0;
     io.to(data).emit("room_count", clientsInRoom);
-    io.emit("user_count", userCount);
+    io.emit("user_count", io.engine.clientsCount);
   });
 
   socket.on("send_message", (data) => {
+    console.log("recv data:",data);
     socket.to(data.room).emit("receive_message", data);
   });
 
   socket.on("disconnect", (data) => {
-    userCount--
+    userCount = io.engine.clientsCount
     console.log("User Disconnected", socket.id);
     const clientsInRoom = io.sockets.adapter.rooms.get(data)?.size ?? 0;
     io.to(data).emit("room_count", clientsInRoom);
